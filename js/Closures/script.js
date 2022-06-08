8fe76c8a3921f20c7ed4803e907ddbba3f47a091
Alex Luca <lucaalexandru06@yahoo.com>
2022-06-08 18:22:54 +0300

    Closures

---
+++ js/Closures/script.js
@@ -0,0 +1,19 @@
+/*
+*   CLOSURES: briefly, functions which return functions! 
+*/
+ 
+function makeMultiplier(multiplier) {
+    // this is the closure
+    return (function (x) {
+        return multiplier * x;
+        }
+    );
+}
+
+var doubleAll = makeMultiplier(2);
+/**
+ * Even if the makeMultiplier() is done, because it returns another function
+ * (it's a closure), the JS engine keep in memory the outer lexical environment and
+ * this is how it knows about the value of multiplier!
+ */
+console.log(doubleAll(10));
