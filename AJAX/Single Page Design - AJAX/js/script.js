22488fbacee4e75e52f4ee6d17b868fc86fc708b
Alex Luca <lucaalexandru06@yahoo.com>
2022-06-11 14:08:51 +0300

    SPA with ajax

---
+++ AJAX/Single Page Design - AJAX/js/script.js
@@ -0,0 +1,38 @@
+
+(function (global) {
+
+var dc = {};
+
+var homeHtml = "snippets/homePage.html";
+
+// Convenience function for inserting innerHTML for 'select'
+var insertHtml = function (selector, html) {
+  var targetElem = document.querySelector(selector);
+  targetElem.innerHTML = html;
+};
+
+// Show loading icon inside element identified by 'selector'.
+var showLoading = function (selector) {
+  var html = " <div> <img src='loading.gif'></div>";
+  
+  insertHtml(selector, html);
+};
+
+// On page load (before images or CSS)
+document.addEventListener("DOMContentLoaded", function (event) {
+
+// On first load, show home view
+showLoading("#main-content");
+$ajaxUtils.sendGetRequest(
+  homeHtml,
+  function (responseText) {
+    document.querySelector("#main-content")
+      .innerHTML = responseText;
+  },
+  false);
+});
+
+
+global.$dc = dc;
+
+})(window);
